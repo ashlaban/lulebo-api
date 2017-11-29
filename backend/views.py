69a435b5f596005f55128088cd27001ada3dc22c
ashlaban <ketost@gmail.com>
2017-11-29 13:08:49 +0100

    update to conform to luleboapi.py changes

--- backend/views.py
+++ backend/views.py
@@ -14,6 +14,8 @@ import werkzeug
 from werkzeug.wrappers import AuthorizationMixin
 
 from luleboapi import LuleboApi
+from luleboapi import LuleboApiError
+from luleboapi import LuleboApiLoginError
 
 
 
