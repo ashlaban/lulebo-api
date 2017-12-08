731a76dbfe6106b3b1abaaa3d96e8d3f5d3426b4
ashlaban <ketost@gmail.com>
2017-12-09 00:57:40 +0100

    Update tests

--- backend/tests/test.py
+++ backend/tests/test.py
@@ -1,8 +1,7 @@
-from flask import Flask
 from flask_testing import TestCase
 
-import os
 import json
+import unittest
 import uuid
 
 import backend.util
@@ -14,14 +13,16 @@ from backend import db
 
 from backend.models import User
 
+
 def default_user():
     return {
-        'username'       : 'test_user',
-        'password'       : 'test_pass',
-        'email'          : '',
-        'lulebo_username': '',
-        'lulebo_password': ''
-    };
+        'username': 'test_user',
+        'password': 'test_pass',
+        'email': 'test@test.com',
+        'lulebo_username': 'lul-test-user',
+        'lulebo_password': 'lul-test-pass'
+    }
+
 
 class MyTestJSON(TestCase):
     '''
@@ -34,20 +35,33 @@ class MyTestJSON(TestCase):
         data         : dict (or compatible object)
         content_type : will be set to json if not provided
         '''
-        try   : kwargs['content_type']
-        except: kwargs['content_type'] = 'application/json'
-        
-        try    : kwargs['data'] = json.dumps(kwargs['data'])
-        except : pass
-        
+        try:
+            kwargs['content_type']
+        except:
+            kwargs['content_type'] = 'application/json'
+
+        try:
+            kwargs['data'] = json.dumps(kwargs['data'])
+        except:
+            pass
+
         return self.client.post(url, **kwargs)
 
     def create_app(self):
         config = {
             'SQLALCHEMY_DATABASE_URI': gen_db_path('test'),
-            'TESTING'                : True
+            'TESTING': True
         }
-        return create_app(config)
+        backend = create_app(config)
+        return backend
+
+    def setUp(self):
+        db.create_all()
+        backend.util.add_user(db, default_user())
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
     def test_hi(self):
         response = self.client.get('/hi')
@@ -66,73 +80,59 @@ class MyTestJSON(TestCase):
 
         payload = dict(username='test_user', password='wrong_pass')
         response = self.post_json('/login', data=payload)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, dict(msg='Wrong username and/or password', status='error'))
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json,
+                         dict(msg='Wrong username and/or password',
+                              status='error'))
 
         payload = dict(username='wrong_user', password='test_pass')
         response = self.post_json('/login', data=payload)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, dict(msg='Wrong username and/or password', status='error'))
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json,
+                         dict(msg='Wrong username and/or password',
+                              status='error'))
 
         payload = dict(username='test_user', password='')
         response = self.post_json('/login', data=payload)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, dict(msg='Wrong username and/or password', status='error'))
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json,
+                         dict(msg='Wrong username and/or password',
+                              status='error'))
 
         payload = dict(username='', password='')
         response = self.post_json('/login', data=payload)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, dict(msg='Wrong username and/or password', status='error'))
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json,
+                         dict(msg='Wrong username and/or password',
+                              status='error'))
 
         payload = dict(password='test_pass')
         response = self.post_json('/login', data=payload)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, dict(msg='Missing username', status='error'))
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json,
+                         dict(msg='Missing username',
+                              status='error'))
 
         payload = dict(username='test_user')
         response = self.post_json('/login', data=payload)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, dict(msg='Missing password', status='error'))
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json,
+                         dict(msg='Missing password',
+                              status='error'))
 
     def test_signup(self):
         username = str(uuid.uuid4())
-        payload = dict(username=username, password='test_pass', email='{}@test.com'.format(username), passvalid='test_pass')
+        payload = dict(username=username,
+                       password='test_pass',
+                       email='{}@test.com'.format(username),
+                       passvalid='test_pass')
         response = self.post_json('/signup', data=payload)
-        print (response.json)
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, dict(msg='Success', status='ok'))
 
         user = User.get_by_name(username)
         self.assertEqual(user.username, username)
-
-
-
-
-class MyTestDB(TestCase):
-    '''
-    This is for the cases where database functionality is required.
-    '''
-
-    def create_app(self):
-        config = {
-            'SQLALCHEMY_DATABASE_URI': gen_db_path('test-regen'),
-            'TESTING'                : True
-        }
-        backend = create_app(config)
-        return backend
-
-    def setUp(self):
-        db.create_all()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    # def test_add_user(self):
-    #     backend.util.add_user(db, default_user())
-        # TODO: Verify that user exists.
-
-    
 
 
 if __name__ == '__main__':
